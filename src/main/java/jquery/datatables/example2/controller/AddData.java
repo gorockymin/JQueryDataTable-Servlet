d416e74d30929546a31b0a1f8b2802d772e04a3a
anderson <Anderson.Wang@quantatw.com>
2015-02-12 11:23:38 +0800

    add uncommit change

---
+++ src/main/java/jquery/datatables/example2/controller/AddData.java
@@ -0,0 +1,47 @@
+package jquery.datatables.example2.controller;
+
+import jquery.datatables.example2.model.Company;
+import jquery.datatables.example2.model.DataRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Handling adding new data
+ */
+@WebServlet("/AddData")
+public class AddData extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddData() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        String town = request.getParameter("town");
+        int country = Integer.parseInt(request.getParameter("country"));
+
+        Company c = new Company(name, address, town, country);
+        DataRepository.GetCompanies().add(c);
+
+        response.getWriter().print(c.getId());
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
---
+++ src/main/java/jquery/datatables/example2/model/Company.java
@@ -0,0 +1,63 @@
+package jquery.datatables.example2.model;
+
+public class Company {
+
+    static int nextID = 17;
+    private int id;
+    private String name;
+    private String address;
+    private String town;
+    private int country;
+
+    public Company(String name, String address, String town) {
+        id = nextID++;
+        this.name = name;
+        this.address = address;
+        this.town = town;
+        this.country = id % 4; //simulation of country id
+    }
+
+    public Company(String name, String address, String town, int country) {
+        id = nextID++;
+        this.name = name;
+        this.address = address;
+        this.town = town;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getCountry() {
+        return country;
+    }
+
+    public void setCountry(int country) {
+        this.country = country;
+    }
+}
